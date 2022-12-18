662ce2f1ef3675fc75fe886050280fb087966fb8
deltaitech <deltaitech>
2022-12-18 10:11:21 +0200

    nbe new edit on transaction id

--- app/Http/Controllers/user/InvoiceFrontController.php
+++ app/Http/Controllers/user/InvoiceFrontController.php
@@ -125,7 +125,7 @@ class InvoiceFrontController extends Controller
 //        $password='c622b7e9e550292df400be7d3e846476';
             $password = '21aaeea3f3a99f36191b7274cdeecb37';
 //        $authUrl = 'https://test-nbe.gateway.mastercard.com/api/rest/version/63/merchant/TESTEGPTEST/order/11'.$invoice->id.'/transaction/1';
-            $authUrl = 'https://nbe.gateway.mastercard.com/api/rest/version/63/merchant/MARWANGROUP/order/11' . $invoice->id . '/transaction/txn-150';
+            $authUrl = 'https://nbe.gateway.mastercard.com/api/rest/version/63/merchant/MARWANGROUP/order/' . $invoice->id . '/transaction/txn-150';
 
             $data_three = array(
                 "apiOperation" => "INITIATE_AUTHENTICATION",
