ab506fd103015e0fd4ec4ac2a4ac8a4276a3d505
deltaitech <deltaitech>
2023-01-23 16:44:04 +0200

    add recaptcha for contacts

--- resources/views/user/pages/contactus.blade.php
+++ resources/views/user/pages/contactus.blade.php
@@ -82,7 +82,6 @@
                                 <div class="row">
                                     <div class="col-xs-12 col-sm-12 col-md-6">
                                         <div class="form-group">
-                                            <strong>Recaptcha:</strong>
                                             {!! NoCaptcha::renderJs() !!}
                                             {!! NoCaptcha::display() !!}
                                         </div>
