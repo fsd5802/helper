f1a78b8eb1e86e20f8505ee7c133c0ac60b5ec7c
deltaitech <deltaitech>
2023-01-24 10:58:43 +0200

    remove delete from contacts

--- resources/views/admin/components/table-control.blade.php
+++ resources/views/admin/components/table-control.blade.php
@@ -1,29 +1,30 @@
-
 @if($name && $value)
 
-<a href="{{getRoute($name.'.show',$value->id    )}}" class="btn btn-sm btn-clean
+    <a href="{{getRoute($name.'.show',$value->id    )}}" class="btn btn-sm btn-clean
 btn-icon mr-2" title="@lang('general.show')">
-    <i class="fa fa-eye"></i>
-</a>
+        <i class="fa fa-eye"></i>
+    </a>
 
-@if (!request()->routeIs('applications.*','service-requests.*','quotations.*'))
-<a href="{{getRoute($name.'.edit',$value->id)}}" class="btn btn-sm btn-clean
+    @if (!request()->routeIs('applications.*','service-requests.*','quotations.*'))
+        <a href="{{getRoute($name.'.edit',$value->id)}}" class="btn btn-sm btn-clean
 btn-icon mr-2 custimizeEditBtn" title="@lang('general.edit')">
-    <i class="fa fa-edit"></i>
-</a>
-@endif
+            <i class="fa fa-edit"></i>
+        </a>
+    @endif
 
-
-<form id="delete-form-{{ $value->id }}" style="display: inline-table;" action="{{ getRoute($name.'.destroy',$value->id) }}"
-    method="post">
-    @csrf
-    @method('delete')
-    <button type="button" class="btn btn-sm btn-clean btn-icon custimizeDelBtn"
-            title="@lang('general.delete')" onclick="confirmDelete
-    ('delete-form-{{ $value->id }}')">
-        <i class="fa fa-trash"></i>
-    </button>
-</form>
+    @if (!request()->routeIs('contacts.*'))
+        <form id="delete-form-{{ $value->id }}" style="display: inline-table;"
+              action="{{ getRoute($name.'.destroy',$value->id) }}"
+              method="post">
+            @csrf
+            @method('delete')
+            <button type="button" class="btn btn-sm btn-clean btn-icon custimizeDelBtn"
+                    title="@lang('general.delete')" onclick="confirmDelete
+                ('delete-form-{{ $value->id }}')">
+                <i class="fa fa-trash"></i>
+            </button>
+        </form>
+    @endif
 @endif
 
 
